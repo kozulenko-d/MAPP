4a100e8a0c102f661f3799051a558b9137d26521
kozulenko_d <geograff86@gmail.com>
2017-09-23 19:50:12 +0300

    23917

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 autoSlider();
-var left = 0;
+var leftMove = 0;
 var timer = 0;
 var line = document.getElementById('slider');
 var frames = document.getElementsByClassName('slide-frame');
@@ -9,6 +9,7 @@ var f3 = document.getElementById('frame3');
 var f4 = document.getElementById('frame4');
 
 f1.onclick = function() {
+	leftMove = 0;
 	line.style.left = 0;
 	this.style.backgroundColor = '#1ab798';
 	f2.style.backgroundColor = '#d4d3d4';
@@ -16,6 +17,7 @@ f1.onclick = function() {
 	f4.style.backgroundColor = '#d4d3d4';
 }
 f2.onclick = function() {
+	leftMove = -frames[0].clientWidth;
 	line.style.left = -frames[0].clientWidth + 'px';
 	this.style.backgroundColor = '#1ab798';
 	f1.style.backgroundColor = '#d4d3d4';
@@ -23,6 +25,7 @@ f2.onclick = function() {
 	f4.style.backgroundColor = '#d4d3d4';
 }
 f3.onclick = function() {
+	leftMove = -(frames[0].clientWidth * 2);
 	line.style.left = -(frames[0].clientWidth * 2) + 'px';
 	this.style.backgroundColor = '#1ab798';
 	f2.style.backgroundColor = '#d4d3d4';
@@ -30,6 +33,7 @@ f3.onclick = function() {
 	f4.style.backgroundColor = '#d4d3d4';
 }
 f4.onclick = function() {
+	leftMove = -(frames[0].clientWidth * 3);
 	line.style.left = -(frames[0].clientWidth * 3) + 'px';
 	this.style.backgroundColor = '#1ab798';
 	f2.style.backgroundColor = '#d4d3d4';
@@ -39,33 +43,33 @@ f4.onclick = function() {
 
 function autoSlider() {
 	timer = setTimeout(function() {
-		left = left - frames[0].clientWidth;
-		if (left < -frames[0].clientWidth*(frames.length - 1)) {
-			left = 0;
+		leftMove = leftMove - frames[0].clientWidth;
+		if (leftMove < -frames[0].clientWidth*(frames.length - 1)) {
+			leftMove = 0;
 			clearTimeout(timer);
 		}
-		if (left >= 0) {
+		if (leftMove >= 0) {
 			f1.style.backgroundColor = '#1ab798';
 			f2.style.backgroundColor = '#d4d3d4';
 			f3.style.backgroundColor = '#d4d3d4';
 			f4.style.backgroundColor = '#d4d3d4';
-		} else if (left >= -(frames[0].clientWidth)) {
+		} else if (leftMove >= -(frames[0].clientWidth)) {
 			f2.style.backgroundColor = '#1ab798';
 			f1.style.backgroundColor = '#d4d3d4';
 			f3.style.backgroundColor = '#d4d3d4';
 			f4.style.backgroundColor = '#d4d3d4';
-		} else if (left >= -(frames[0].clientWidth * 2)) {
+		} else if (leftMove >= -(frames[0].clientWidth * 2)) {
 			f3.style.backgroundColor = '#1ab798';
 			f2.style.backgroundColor = '#d4d3d4';
 			f1.style.backgroundColor = '#d4d3d4';
 			f4.style.backgroundColor = '#d4d3d4';
-		} else if (left >= -(frames[0].clientWidth * 3)) {
+		} else if (leftMove >= -(frames[0].clientWidth * 3)) {
 			f4.style.backgroundColor = '#1ab798';
 			f2.style.backgroundColor = '#d4d3d4';
 			f3.style.backgroundColor = '#d4d3d4';
 			f1.style.backgroundColor = '#d4d3d4';
 		}
-		line.style.left = left + 'px';
+		line.style.left = leftMove + 'px';
 		autoSlider();
 	}, 12000)
 }
